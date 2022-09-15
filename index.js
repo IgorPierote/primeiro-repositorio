021e14ad6fc506e389a46507350f4c23072303c7
IgorPierote <pieroteigor@gmail.com>
2022-09-15 17:55:11 -0300

    Aula da tarde att

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+--> Váriaveis no Javascript
+--> Operadores matemáticos do Javascript
+*/
+// Serve para escrever mensagem no console
+console.log('Um forastero!')
+console.log(2345678)
+console.log(43.53)
+console.log(true)
+console.log(false)
+
+// Váriaveis
+
+let mensagem = "Um forastero"
+
+console.log(mensagem)
+
+mensagem = "Cabron"
+
+console.log(mensagem)
+
+
+
